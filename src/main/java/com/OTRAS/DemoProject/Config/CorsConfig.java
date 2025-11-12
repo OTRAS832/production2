ab19f354b836c3e234d8dcdb36956ee3674ea27d
OTRAS832 <otras832@gmail.com>
2025-11-12 08:55:56 +0530

    Update CorsConfig.java

--- src/main/java/com/OTRAS/DemoProject/Config/CorsConfig.java
+++ src/main/java/com/OTRAS/DemoProject/Config/CorsConfig.java
@@ -1,37 +1,37 @@
-package com.OTRAS.DemoProject.Config;
+// package com.OTRAS.DemoProject.Config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
+// @Configuration
+// public class CorsConfig {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        // .allowedOrigins(
-                        //         "http://localhost:5171",
-                        //         "http://localhost:5172",
-                        //         "http://localhost:5173",
-                        //         "http://localhost:5174",
-                        //         "http://localhost:5175",
-                        //         "https://otras-admin-h5q6.vercel.app",
-                        //         "https://otrasuser.vercel.app",
-                        //         "https://otras-exam.vercel.app"
-                        // )
-                      .allowedOriginPatterns(
-                                "http://localhost:*",
-                                "https://*.vercel.app"
-                        )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry.addMapping("/**")
+//                         // .allowedOrigins(
+//                         //         "http://localhost:5171",
+//                         //         "http://localhost:5172",
+//                         //         "http://localhost:5173",
+//                         //         "http://localhost:5174",
+//                         //         "http://localhost:5175",
+//                         //         "https://otras-admin-h5q6.vercel.app",
+//                         //         "https://otrasuser.vercel.app",
+//                         //         "https://otras-exam.vercel.app"
+//                         // )
+//                       .allowedOriginPatterns(
+//                                 "http://localhost:*",
+//                                 "https://*.vercel.app"
+//                         )
+//                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                         .allowedHeaders("*")
+//                         .allowCredentials(true);
+//             }
+//         };
+//     }
+// }
