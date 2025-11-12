46d2729f124f370eec7bf0d24f501c6e0dc9f0c9
otras832 <otras832@gmail.com>
2025-11-12 07:20:10 +0530

    updated config

--- src/main/java/com/OTRAS/DemoProject/Config/CorsConfig.java
+++ src/main/java/com/OTRAS/DemoProject/Config/CorsConfig.java
@@ -1,7 +1,5 @@
 package com.OTRAS.DemoProject.Config;
 
-import java.util.Arrays;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -16,14 +14,16 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                      //  .allowedOriginPatterns("http://localhost:5173") 
-                .allowedOrigins(
-                        "http://localhost:5171",
-                        "http://localhost:5172",
-                        "http://localhost:5173",
-                        "http://localhost:5174",
-                        "http://localhost:5175"
-                    )
+                        .allowedOrigins(
+                                "http://localhost:5171",
+                                "http://localhost:5172",
+                                "http://localhost:5173",
+                                "http://localhost:5174",
+                                "http://localhost:5175",
+                                "https://otras-admin-h5q6.vercel.app",
+                                "https://otrasuser.vercel.app",
+                                "https://otras-exam.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
